39c45061d9b7684c196d802c0cb657a39344aa66
Balazs <andras.balazs@accenture.com>
2017-09-19 17:02:23 +0200

    lesson 2 finished

--- Space Shooter/Assets/_Scripts/DestroyByContact.cs
+++ Space Shooter/Assets/_Scripts/DestroyByContact.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion = null;
+	public GameObject playerExplosion  = null;
+
 	void OnTriggerEnter(Collider other){
 
 		//if(other.name != "Boundary"){
 		if(other.tag == "Boundary"){
 			return;        	
+		}
+		Instantiate(explosion, transform.position, transform.rotation);
+		if(other.tag == "Player"){
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);	
 		}
         Destroy(other.gameObject);
 		Destroy(gameObject);	
